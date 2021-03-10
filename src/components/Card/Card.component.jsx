f3223617f37ba6769679f47747e28b1baf76cb6a
Sakshi Gairola <1906499@kiit.ac.in>
2021-03-10 23:46:20 +0530

    making minor changes for generating dynamic links

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -6,7 +6,7 @@ const Card = props => (
         <div className='h-18 font-serif text-xs p-2 mb-6'>
             {props.description}
         </div>
-        <a href="#" className='text-sm sm:text-base shadow-lg bg-blue-100 rounded px-3 py-2 text-blue-900'>View On Github</a>
+        <a href={props.link} className='text-sm sm:text-base shadow-lg bg-blue-100 rounded px-3 py-2 text-blue-900'>View On Github</a>
     </div>
 );
 
