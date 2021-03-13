4455096369c6e0ce600457aa1444d6eece85262f
grsakshi <sakshi.g642001@gmail.com>
2021-03-13 22:33:52 +0530

    completed blog section component and adding extensions for some tags in tailwind config

--- src/components/Blog/blog.component.jsx
+++ src/components/Blog/blog.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Blog = props => (
-    <div className='w-96 h-48 flex flex-row bg-black'>
+    <div className='w-big h-48 flex flex-row bg-black'>
         <img src={props.imageUrl} alt="blogpics" height='190' className='object-cover h-48 w-52' />
 
-        <div className='w-44 h-48 text-gray-300'>
-            <span>{props.date}</span>
-            <h3 className=''>{props.title}</h3>
-            <a href={props.link}>READ MORE</a>
+        <div className='w-68 h-48 text-gray-200 pt-10 text-left pl-2 overflow-hidden'>
+            <span className='text-xxs'>{props.date}</span>
+            <h3 className=' text-2xl font-bold text-gray-50 pb-2'>{props.title}</h3>
+            <a href={props.link} className='text-xxs bg-gray-300 text-gray-900 font-bold rounded-lg px-2 py-1 shadow-md'>READ MORE</a>
         </div>
     </div>
 );
--- src/components/BlogSection/blogsection.component.jsx
+++ src/components/BlogSection/blogsection.component.jsx
@@ -10,28 +10,28 @@ class BlogSection extends React.Component{
                 {
                     id: 1,
                     imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=701&q=80',
-                    date: '',
+                    date: '12th APRIL 2020',
                     title: 'Title 1',
                     link: ''
                 },
                 {
                     id: 2,
                     imageUrl: 'https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80',
-                    date: '',
+                    date: '12th MARCH 2020',
                     title: 'Title 2',
                     link: ''
                 },
                 {
                     id: 3,
                     imageUrl: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80',
-                    date: '',
-                    title: 'Title 3',
+                    date: '12th OCTOBER 2020',
+                    title: '9 ways to trick your brain into working',
                     link: ''
                 },
                 {
                     id: 4,
                     imageUrl: 'https://images.unsplash.com/photo-1529397938791-2aba4681454f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-                    date: '',
+                    date: '12th MARCH 2020',
                     title: 'Title 4',
                     link: ''
                 }
